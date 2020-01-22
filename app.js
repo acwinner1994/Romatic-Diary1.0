89f9a821998ef1e956631c8b956c6933e9213092
albert chingis <albertchingis@alberts-MBP.fios-router.home>
2020-01-22 18:01:34 -0500

    final1

--- app.js
+++ app.js
@@ -172,6 +172,6 @@ app.post("/set_lover",function(req,res){
     });
 });
 
-app.listen(3000, function() {
-  console.log("Server started on port 3000");
+app.listen(process.env.PORT||3000, function(){
+   console.log("The Server Has Started!");
 });
